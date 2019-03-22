880c3a306238317a351c1407f5ae585ee7b302ae
Christian Henry <chenry5724@gmail.com>
2019-03-21 22:09:57 -0500

    first

---
+++ brody/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+        <header className="App-header">
+         DogsWithFriends
+         
+        </header>
+        <div id="motto">Where Dogs and Human Friends Meet
+        </div>
+        <p id="otto"> Future Development coming soon</p>
+      </div>
+    );
+  }
+}
+
+export default App;
